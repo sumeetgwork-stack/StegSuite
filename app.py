59909186c5be0ab477a7de3fdf7adde3cf08dfd1
sumeetgwork-stack <sumeetgwork@gmail.com>
2026-09-06 14:01:26 +0530

    Fix: add db.create_all() to create tables on startup

--- app.py
+++ app.py
@@ -28,6 +28,8 @@ os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
 
 db.init_app(app)
 
+with app.app_context():
+    db.create_all()
 login_manager = LoginManager()
 login_manager.login_view = 'login'
 login_manager.init_app(app)
